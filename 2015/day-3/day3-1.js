ff7bd82766050a9d27a915f0fb0919bdc740ac6d
maximiliansteiger <maximiliansteiger15@gmail.com>
2022-08-31 23:55:57 +0200

    2015/3/1-2

--- 2015/day-3/day3-1.js
+++ 2015/day-3/day3-1.js
@@ -1,5 +1,16 @@
 const { input } = require('./readData');
 
+let arr = input.split("");
 
+let posX = 0;
+let posY = 0;
+let visited = {};
+let count = 0;
 
-// ------------------not done in js yet---------------------------------
+for (const move of arr) {
+    posX += move === '>' ? 1 : move === '<' ? -1 : 0;
+    posY += move === '^' ? 1 : move === 'v' ? -1 : 0;
+    visited[`${posX},${posY}`] = ++count;
+}
+
+console.log(Object.keys(visited).length+1); //plus 1 because we start at 0,0
--- 2015/day-3/day3-2.js
+++ 2015/day-3/day3-2.js
@@ -1,5 +1,31 @@
-const { input } = require('./readData');
+const {
+    input
+} = require('./readData');
+
+let arr = input.split("");
+
+let humanPosX = 0;
+let humanPosY = 0;
+let robotPosX = 0;
+let robotPosY = 0;
 
 
+let visited = {};
+let count = 2;
 
-// ------------------not done in js yet---------------------------------
+let robo = false;
+
+for (const move of arr) {
+    if (robo) {
+        robotPosX += move === '>' ? 1 : move === '<' ? -1 : 0;
+        robotPosY += move === '^' ? 1 : move === 'v' ? -1 : 0;
+        visited[`${robotPosX},${robotPosY}`] = ++count;
+    } else {
+        humanPosX += move === '>' ? 1 : move === '<' ? -1 : 0;
+        humanPosY += move === '^' ? 1 : move === 'v' ? -1 : 0;
+        visited[`${humanPosX},${humanPosY}`] = ++count;
+    }
+    robo = !robo;
+}
+
+console.log(Object.keys(visited).length); //plus 1 because we start at 0,0
