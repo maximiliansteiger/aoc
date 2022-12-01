8e882451ccd1848618c1615ef739f3fd67258361
Maximilian Steiger <maximiliansteiger15@gmail.com>
2022-12-01 06:13:40 +0100

    add 2022/1 1&2

--- 2022/day-1/part1.js
+++ 2022/day-1/part1.js
@@ -1,3 +1,18 @@
 const { input } = require('./readData');
 
-let arr = input.split("\r\n")
+let arr = input.split("\r\n").map(Number)
+
+let maxCNT = 0;
+let cnt = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+        if (cnt > maxCNT) {
+            maxCNT = cnt;
+        }
+        cnt = 0;
+    }
+    cnt += arr[i]
+}
+
+console.log(maxCNT);
--- 2022/day-1/part2.js
+++ 2022/day-1/part2.js
@@ -1,2 +1,18 @@
 const { input } = require('./readData');
 
+let arr = input.split("\r\n").map(Number)
+
+let cntArray = [];
+let cnt = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+        cntArray.push(cnt);
+        cnt = 0;
+    }
+    cnt += arr[i]
+}
+
+cntArray.sort((a, b) => b - a);
+
+console.log(cntArray[0] + cntArray[1] + cntArray[2]);
