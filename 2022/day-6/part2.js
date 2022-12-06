995c30d08600c344bc0fcd438bc8326392356f41
Maximilian Steiger <maximiliansteiger15@gmail.com>
2022-12-06 07:21:12 +0100

    add 2022/6 1&2

---
+++ 2022/day-6/part2.js
@@ -0,0 +1,11 @@
+const { input } = require('./readData');
+
+let signals = input.split("");
+let n = 14;
+for (let i = 0; i < signals.length; i++) {
+    let firstFour = signals.slice(i, i + n);
+    if (firstFour.length === new Set(firstFour).size) {
+        console.log(i + n);
+        break;
+    }
+}
